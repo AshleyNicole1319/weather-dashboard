9cf10effa518332732c28b864a8898a602256182
AshleyNicole1319 <a-warford96@comcast.net>
2022-07-19 20:42:47 -0400

    connect weather api

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,31 @@ var displayFiveEl = document.querySelector("#fivedisplay")
 var saveSearch = function(){
     localStorage.setItem("cities", JSON.stringify(cities));
 }
+
+//Connect API
+var cityWeather = function(city){
+    var apiKey = "1767dcfe210ef96ad104c047ad61f1bb";
+    var apiUrl =  ("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey);
+
+
+fetch(apiUrl).then(function(response){
+    if(response.ok){
+        console.log(response);
+        response.json().then(function(data){
+            console.log(data);
+            getWeather(data,city);
+        });
+    }else{
+        alert("Error");
+    }   
+})
+.catch(function(error){
+    alert("Unable to connect");
+});
+};
+
+//Form Stuff
+
+//Weather Variables
+
+//Make Action Work on "Submit"
